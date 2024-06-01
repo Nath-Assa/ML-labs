050bb76ac1d6b667621d8bb14a9204d4ce97d370
Nathaniel <126282075+Nath-Assa@users.noreply.github.com>
2024-06-01 19:19:47 +0200

    Update deep_network.py

--- sciper1_sciper2_sciper3_project/src/methods/deep_network.py
+++ sciper1_sciper2_sciper3_project/src/methods/deep_network.py
@@ -13,7 +13,7 @@ class MLP(nn.Module):
     It should not use any convolutional layers.
     """
 
-    def __init__(self, input_size, n_classes):
+    def __init__(self, input_size, n_classes, hidden_size=128):
         """
         Initialize the network.
         
@@ -25,11 +25,11 @@ class MLP(nn.Module):
             n_classes (int): number of classes to predict
         """
         super().__init__()
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, n_classes)
+        self.dropout = nn.Dropout(0.5) 
 
     def forward(self, x):
         """
@@ -41,11 +41,11 @@ class MLP(nn.Module):
             preds (tensor): logits of predictions of shape (N, C)
                 Reminder: logits are value pre-softmax.
         """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        preds = self.fc3(x)
         return preds
 
 
